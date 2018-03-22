1bb4fe3b287f4db6a26c2221fe674e8cd4c2613b
xiaoxiao <242799999@qq.com>
2018-03-22 11:18:03 +0800

    支付接口

---
+++ 彩小蜜/Classes/Section/Payment/NetAPIManager/PaymentNetAPIManager.swift
@@ -0,0 +1,91 @@
+//
+//  PaymentNetAPIManager.swift
+//  彩小蜜
+//
+//  Created by HX on 2018/3/22.
+//  Copyright © 2018年 韩笑. All rights reserved.
+//
+
+import Moya
+import RxSwift
+
+let paymentProvider = MoyaProvider<PaymentNetAPIManager>(plugins:[RequestLoadingPlugin()])
+
+enum PaymentNetAPIManager {
+    /// 支付
+    case payment (payCode: String, payToken: String)
+    /// 支付订单结果
+    case paymentQuery
+    /// 充值
+    case paymentRecharge (payCode: String, totalAmount: String)
+    /// 提现
+    case paymentWithdraw (totalAmount: String, userBankId: String)
+}
+
+extension PaymentNetAPIManager : TargetType {
+    var baseURL : URL {
+        return URL(string : baseURLStr)!
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type" : "application/json",
+                "token" : UserInfoManager().getToken()
+        ]
+    }
+    
+    var path : String {
+        switch self {
+        case .payment:
+            return "/payment/app"
+        case .paymentQuery:
+            return "/payment/query"
+        case .paymentRecharge:
+            return "/payment/recharge"
+        case .paymentWithdraw:
+            return "/payment/withdraw"
+            
+            
+        }
+    }
+    
+    var task: Task {
+        var dic : [String: Any] = [:]
+        
+        switch self {
+        case .payment(let payCode, let payToken):
+            dic["payCode"] = payCode
+            dic["payToken"] = payToken
+        case .paymentRecharge(let payCode, let totalAmount):
+            dic["payCode"] = payCode
+            dic["totalAmount"] = totalAmount
+        case .paymentWithdraw(let totalAmount, let userBankId):
+            dic["totalAmount"] = totalAmount
+            dic["userBankId"] = userBankId
+        default:
+            return .requestPlain
+        }
+        
+        let jsonStr = try? JSONSerialization.data(withJSONObject: dic, options: .prettyPrinted)
+        
+        
+        return .requestData(jsonStr!)
+    }
+    
+    var method : Moya.Method {
+        switch self {
+        default:
+            return .post
+        }
+    }
+    
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+        default:
+            return URLEncoding.default
+        }
+    }
+    
+    var sampleData: Data {
+        return "{}".data(using: String.Encoding.utf8)!
+    }
+}
