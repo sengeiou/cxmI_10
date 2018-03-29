702cc408fec062f2d010a40a971d26c84574dd87
xiaoxiao <242799999@qq.com>
2018-03-29 19:42:45 +0800

    11

---
+++ 彩小蜜/Classes/Section/Home/FootballMatch/Controller/FootballMatchVC.swift
@@ -0,0 +1,30 @@
+//
+//  FootballMatchVC.swift
+//  彩小蜜
+//
+//  Created by HX on 2018/3/29.
+//  Copyright © 2018年 韩笑. All rights reserved.
+//
+
+import UIKit
+
+enum FootballMatchType: String {
+    case 胜平负 = "彩小秘 · 胜平负"
+}
+
+class FootballMatchVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    
+
+}
