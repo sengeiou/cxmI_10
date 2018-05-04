e722fc0b3b1ec6bf5b3105277f7ae1403b30f151
hanxiao <242799999@qq.com>
2018-05-04 18:38:46 +0800

    支付，，完善状态

--- 彩小蜜/Classes/Section/Payment/Controller/PaymentViewController.swift
+++ 彩小蜜/Classes/Section/Payment/Controller/PaymentViewController.swift
@@ -48,8 +48,6 @@ class PaymentViewController: BaseViewController, UITableViewDelegate, UITableVie
         WeixinCenter.share.payDelegate = self
         initSubview()
         
-
-        
         timer = Timer(timeInterval: 3, target: self, selector: #selector(pollingStart), userInfo: nil, repeats: true)
         
         NotificationCenter.default.addObserver(self, selector: #selector(startPollingTimer), name: NSNotification.Name(rawValue: NotificationWillEnterForeground), object: nil)
@@ -74,6 +72,7 @@ class PaymentViewController: BaseViewController, UITableViewDelegate, UITableVie
             timer.invalidate()
             SVProgressHUD.dismiss()
             showCXMCancelAlert(title: "查询失败", message: "暂未查询到您的支付结果，如果您已经确认支付并成功扣款，可能存在延迟到账的情况，请到账户明细中查看或联系客服查询", action: "知道了") { (action) in
+                self.canPayment = true
             }
             return
         }
@@ -187,12 +186,28 @@ class PaymentViewController: BaseViewController, UITableViewDelegate, UITableVie
             .asObservable()
             .mapBaseObject(type: DataModel.self)
             .subscribe(onNext: { (data) in
-                if data.code == "0" {
+                switch data.code {
+                case "0":
                     self.canPayment = true
                     self.timer.invalidate()
                     self.showHUD(message: data.msg)
                     SVProgressHUD.dismiss()
+                    let order = OrderDetailVC()
+                    order.backType = .root
+                    order.orderId = self.paymentResult.orderId
+                    self.pushViewController(vc: order)
+                case "304035":
+                    self.canPayment = true
+                    self.showHUD(message: data.msg)
+                    self.showCXMCancelAlert(title: "支付失败", message: "如果您已经确认支付并成功扣款，可能存在延迟到账的情况，请到账户明细中查看或联系客服查询", action: "知道了", confirm: { (action) in
+                        self.canPayment = true
+                    })
+                    SVProgressHUD.dismiss()
+                    break
+                    
+                default: break
                 }
+                
             }, onError: { (error) in
                 guard let err = error as? HXError else { return }
                 switch err {
