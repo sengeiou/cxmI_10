ce8438b2558bd52f6e611dd006e14b5db13843de
hanxiao <242799999@qq.com>
2018-05-08 11:34:16 +0800

    订单确认，，更改 能否进行下一步的逻辑，

--- 彩小蜜/Classes/Section/Home/FootballMatch/Protocol/FootballOrderProtocol.swift
+++ 彩小蜜/Classes/Section/Home/FootballMatch/Protocol/FootballOrderProtocol.swift
@@ -200,7 +200,7 @@ extension FootballOrderProtocol where Self: FootballOrderConfirmVC {
                 self.dismissProgressHud()
                 weakSelf?.betInfo = data
                
-                if data.showMsg == nil || data.showMsg == "" || data.maxLotteryMoney <= 20000 || data.betNum <= 10000 {
+                if (data.showMsg == nil || data.showMsg == "") && (data.maxLotteryMoney <= 20000 && data.betNum <= 10000) {
                     weakSelf?.canPush = true
                 }else {
                     weakSelf?.canPush = false
