da8d8b98fa777c55f65ab71868501a23bf942743
hanxiao <242799999@qq.com>
2018-05-30 12:26:55 +0800

    世界杯订单详情

--- Source/Classes/Section/Me/PurchaseRecord/View/WorldCupOrderDetailTitleCell.swift
+++ Source/Classes/Section/Me/PurchaseRecord/View/WorldCupOrderDetailTitleCell.swift
@@ -10,11 +10,9 @@ import UIKit
 
 class WorldCupOrderDetailTitleCell: UITableViewCell {
 
-    private var timeTitle : UILabel!
     private var nameTitle: UILabel!
-    private var ruleTitle: UILabel!    // 玩法
-    private var recordTitle: UILabel!  // 投注
-    private var resultTitle: UILabel!  // 赛果
+    private var oddTitle: UILabel!      // 玩法
+    private var resultTitle: UILabel!    // 赛果
     
     private var sectionTitle : UILabel!
     private var line : UIView!
@@ -27,9 +25,6 @@ class WorldCupOrderDetailTitleCell: UITableViewCell {
     }
     override func layoutSubviews() {
         super.layoutSubviews()
-        
-        
-        
     }
     
     private func initSubview() {
@@ -43,24 +38,16 @@ class WorldCupOrderDetailTitleCell: UITableViewCell {
         sectionTitle.textColor = ColorA0A0A0
         sectionTitle.textAlignment = .left
         sectionTitle.text = "方案内容"
-        
-        timeTitle = getTitleLB("场次")
-        timeTitle.textAlignment = .left
         nameTitle = getTitleLB("赛事")
-        ruleTitle = getTitleLB("玩法")
-        
-        recordTitle = getTitleLB("投注")
+        oddTitle = getTitleLB("玩法")
         resultTitle = getTitleLB("赛果")
         resultTitle.textAlignment = .right
         
         self.contentView.addSubview(line)
         self.contentView.addSubview(sectionTitle)
-        self.contentView.addSubview(timeTitle)
         self.contentView.addSubview(nameTitle)
-        self.contentView.addSubview(ruleTitle)
-        self.contentView.addSubview(recordTitle)
+        self.contentView.addSubview(oddTitle)
         self.contentView.addSubview(resultTitle)
-        
         
         line.snp.makeConstraints { (make) in
             make.top.equalTo(self.contentView).offset(orderSectionHeaderHeight)
@@ -68,40 +55,26 @@ class WorldCupOrderDetailTitleCell: UITableViewCell {
             make.right.equalTo(self.contentView).offset(-SeparatorLeftSpacing)
             make.height.equalTo(SeparationLineHeight)
         }
-        
         sectionTitle.snp.makeConstraints { (make) in
             make.top.equalTo(self.contentView).offset(11)
             make.bottom.equalTo(line.snp.top).offset(-11)
             make.left.equalTo(self.contentView).offset(leftSpacing)
             make.right.equalTo(self.contentView).offset(-rightSpacing)
         }
-        
-        timeTitle.snp.makeConstraints { (make) in
+        nameTitle.snp.makeConstraints { (make) in
             make.top.equalTo(line.snp.bottom).offset(11)
             make.left.equalTo(self.contentView).offset(26)
             make.width.equalTo(40 * defaultScale)
-            //make.height.equalTo(12)
             make.bottom.equalTo(-11)
         }
-        nameTitle.snp.makeConstraints { (make) in
-            make.top.height.equalTo(timeTitle)
-            make.left.equalTo(timeTitle.snp.right).offset(1)
-            //make.right.equalTo(ruleTitle.snp.left).offset(-1)
-        }
-        ruleTitle.snp.makeConstraints { (make) in
-            make.top.height.equalTo(timeTitle)
+        oddTitle.snp.makeConstraints { (make) in
+            make.top.height.equalTo(nameTitle)
             make.width.equalTo(nameTitle)
             make.left.equalTo(nameTitle.snp.right).offset(1)
-            make.right.equalTo(recordTitle.snp.left).offset(-1)
+            make.right.equalTo(nameTitle.snp.left).offset(-1)
         }
-        recordTitle.snp.makeConstraints { (make) in
-            make.top.height.equalTo(timeTitle)
-            make.width.equalTo(OrderDetailTitleWidth)
-            make.right.equalTo(resultTitle.snp.left).offset(-1)
-        }
-        
         resultTitle.snp.makeConstraints { (make) in
-            make.top.height.equalTo(timeTitle)
+            make.top.height.equalTo(nameTitle)
             make.width.equalTo(OrderDetailTitleWidth - 30)
             make.right.equalTo(self.contentView).offset(-26)
         }
@@ -122,7 +95,6 @@ class WorldCupOrderDetailTitleCell: UITableViewCell {
         lab.font = Font12
         lab.textColor = Color505050
         lab.textAlignment = .center
-        //lab.backgroundColor = ColorEA5504
         return lab
     }
     
