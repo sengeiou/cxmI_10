57bc71fbcc20f3ebdca1400a37b812d080d7544b
hanxiao <242799999@qq.com>
2018-06-03 15:44:49 +0800

    充值送

--- Source/Classes/Section/Activity/Controller/ActivityRechargeResultVC.swift
+++ Source/Classes/Section/Activity/Controller/ActivityRechargeResultVC.swift
@@ -16,6 +16,7 @@ class ActivityRechargeResultVC: BasePopViewController {
 
     public var rechargeAmount : String! {
         didSet{
+            guard rechargeAmount != nil else { return }
             let amountAtt = NSMutableAttributedString(string: "恭喜!\n", attributes: [NSAttributedStringKey.font: Font36, NSAttributedStringKey.foregroundColor : ColorF6AD41])
             let title = NSAttributedString(string: "彩小秘恭喜您通过充值活动\n", attributes: [NSAttributedStringKey.font: Font16, NSAttributedStringKey.foregroundColor : ColorFFFFFF])
             let amount = NSMutableAttributedString(string: "获得\(rechargeAmount!)元彩金", attributes: [NSAttributedStringKey.font: Font25, NSAttributedStringKey.foregroundColor : ColorF6AD41])
@@ -51,13 +52,13 @@ class ActivityRechargeResultVC: BasePopViewController {
     // MARK: - 网络请求
     private func receiveRechargeBonusRequest() {
         guard self.payLogId != nil else { return }
-        weak var weakSelf = self
+        
         
         _ = activityProvider.rx.request(.receiveRechargeBonus(payLogId: self.payLogId))
             .asObservable()
             .mapObject(type: ReceiveRechargeBonusModel.self)
             .subscribe(onNext: { (data) in
-                
+                self.rechargeAmount = data.donationPrice
             }, onError: { (error) in
                 print(error)
                 guard let err = error as? HXError else { return }
