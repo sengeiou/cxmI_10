fd2ae0bbc9a79d8b8421ddda8fabdc8288f92237
hanxiao <242799999@qq.com>
2018-07-06 18:43:37 +0800

    1.0.8 需求：查看详情 分析，未来赛事

---
+++ Source/Classes/Section/Home/FootballMatchTeamInfo/View/FootballMatchInfoFutureCell.swift
@@ -0,0 +1,24 @@
+//
+//  FootballMatchInfoFutureCell.swift
+//  彩小蜜
+//
+//  Created by 笑 on 2018/7/6.
+//  Copyright © 2018年 韩笑. All rights reserved.
+//
+
+import UIKit
+
+class FootballMatchInfoFutureCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Source/Classes/Section/Home/FootballMatchTeamInfo/View/FootballMatchInfoFutureHeader.swift
@@ -0,0 +1,21 @@
+//
+//  FootballMatchInfoFutureHeader.swift
+//  彩小蜜
+//
+//  Created by 笑 on 2018/7/6.
+//  Copyright © 2018年 韩笑. All rights reserved.
+//
+
+import UIKit
+
+class FootballMatchInfoFutureHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
