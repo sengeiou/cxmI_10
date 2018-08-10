7a4affcb4a3006f939024a16d00ffac480bf7955
hanxiao <242799999@qq.com>
2018-08-10 16:16:33 +0800

    红球冷热，，篮球冷热

--- Source/Classes/Section/Daletou/TrendViews/DLTTrendBottom.swift
+++ Source/Classes/Section/Daletou/TrendViews/DLTTrendBottom.swift
@@ -9,18 +9,72 @@
 import UIKit
 
 class DLTTrendBottom: UIView {
-    @IBOutlet weak var redCollectionView : DLTTrendBallCollectionView!
-    @IBOutlet weak var blueCollectionView : DLTTrendBallCollectionView!
+    @IBOutlet weak var redCollectionView : UICollectionView!
+    @IBOutlet weak var blueCollectionView : UICollectionView!
+    
+    private var redList : [DaletouDataModel]!
+    private var blueList : [DaletouDataModel]!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+//        self.redCollectionView.delegate = self
+//        self.blueCollectionView.delegate = self
     }
 }
 
 extension DLTTrendBottom {
     public func configure(red : [DaletouDataModel], blue : [DaletouDataModel]) {
-        self.redCollectionView.configure(with: red)
-        self.blueCollectionView.configure(with: blue)
+        self.redList = red
+        self.blueList = blue
+        self.redCollectionView.reloadData()
+        self.blueCollectionView.reloadData()
+    }
+}
+
+extension DLTTrendBottom : UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == self.redCollectionView {
+            redList[indexPath.row].selected = !redList[indexPath.row].selected
+            collectionView.reloadData()
+        }else if collectionView == self.blueCollectionView {
+            blueList[indexPath.row].selected = !blueList[indexPath.row].selected
+            collectionView.reloadData()
+        }
+    }
+}
+
+extension DLTTrendBottom : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.redCollectionView {
+            return redList != nil ? redList.count : 0
+        }
+        else if collectionView == self.blueCollectionView {
+            return blueList != nil ? blueList.count : 0
+        }
+        else {
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DLTTrendBallItem", for: indexPath) as! DLTTrendBallItem
+        
+        if collectionView == self.redCollectionView {
+            cell.configure(with: redList[indexPath.row])
+        }
+        else if collectionView == self.blueCollectionView {
+            cell.configure(with: blueList[indexPath.row])
+        }
+        
+        
+        return cell
+    }
+    
+    
+}
+
+extension DLTTrendBottom : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: DLTTrendBallItem.width, height: DLTTrendBallItem.heiht)
     }
 }
