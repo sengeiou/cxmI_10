df555f8223ba4b5bcde873356b063c7c1e728c1f
hanxiao <242799999@qq.com>
2018-08-14 12:09:01 +0800

    走势图设置

--- Source/Classes/Section/Daletou/Controllers/CXMMDLTTrendSettingVC.swift
+++ Source/Classes/Section/Daletou/Controllers/CXMMDLTTrendSettingVC.swift
@@ -2,13 +2,13 @@
 //  CXMMDLTTrendSettingVC.swift
 //  彩小蜜
 //
-//  Created by 笑 on 2018/8/13.
+//  Created by 笑 on 2018/8/14.
 //  Copyright © 2018年 韩笑. All rights reserved.
 //
 
 import UIKit
 
-class CXMMDLTTrendSettingVC: BasePopViewController {
+class CXMMDLTTrendSettingVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
