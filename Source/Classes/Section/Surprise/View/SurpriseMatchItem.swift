a97d6c03782f21bb41855ada9ffa1d52f6212eb9
hanxiao <242799999@qq.com>
2018-08-28 11:03:27 +0800

    发现改版

---
+++ Source/Classes/Section/Surprise/View/SurpriseMatchItem.swift
@@ -0,0 +1,15 @@
+//
+//  SurpriseMatchItem.swift
+//  彩小蜜
+//
+//  Created by 笑 on 2018/8/28.
+//  Copyright © 2018年 韩笑. All rights reserved.
+//
+
+import UIKit
+
+class SurpriseMatchItem: UICollectionViewCell {
+    
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var title: UILabel!
+}
---
+++ Source/Classes/Section/Surprise/View/SurpriseShooterHeader.swift
@@ -0,0 +1,25 @@
+//
+//  SurpriseShooterHeader.swift
+//  彩小蜜
+//
+//  Created by 笑 on 2018/8/28.
+//  Copyright © 2018年 韩笑. All rights reserved.
+//
+
+import UIKit
+
+class SurpriseShooterHeader: UITableViewHeaderFooterView {
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        initSubview()
+    }
+    
+    private func initSubview() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
