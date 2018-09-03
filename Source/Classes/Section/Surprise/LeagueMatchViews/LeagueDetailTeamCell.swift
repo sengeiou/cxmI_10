9928f31866e7647051fe846e87c96de906bc1241
hanxiao <242799999@qq.com>
2018-09-03 14:49:38 +0800

    联赛，详情 赛程安排

---
+++ Source/Classes/Section/Surprise/LeagueMatchViews/LeagueDetailTeamCell.swift
@@ -0,0 +1,32 @@
+//
+//  LeagueDetailTeamCell.swift
+//  彩小蜜
+//
+//  Created by 笑 on 2018/9/3.
+//  Copyright © 2018年 韩笑. All rights reserved.
+//
+
+import UIKit
+
+class LeagueDetailTeamCell: UITableViewCell {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
+
+extension LeagueDetailTeamCell {
+    public func configure() {
+        
+    }
+}
